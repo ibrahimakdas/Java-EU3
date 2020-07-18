9ba335228ab6cbc6fef70c487adb8b011d3f9de9
europe <europe@192.168.1.153>
2020-07-18 15:32:31 -0400

    EU3

---
+++ src/review_sessions/week8/TwoDArray.java
@@ -0,0 +1,48 @@
+package review_sessions.week8;
+
+import java.util.Arrays;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		
+		String[][] list = new String[4][3];
+		
+		//row1
+		list[0][0]="Potatoes";
+		list[0][1]="Carrrots";
+		list[0][2]="Onions";
+		
+		
+		//row2
+		list[1][0]="Apples";
+		list[1][1]="Kiwis";
+		list[1][2]="Figs";
+		
+		
+		//row3
+		list[2][0]="Milk";
+		list[2][1]="Egg";
+		list[2][2]="Cheese";
+		
+		//row4
+		list[3][0]="Bread";
+		list[3][1]="Bagel";
+		list[3][2]="Muffin";
+		
+		/*
+		 0 - > "Potatoes","Carrots","Onions"
+		 1 - > "Apples" , "Kiwis" , "Figs"
+		 2 - > "Milk", "Egg" , "Cheese"
+		 3 - > "Bread" , "Bagel" , "Muffin"
+		 */
+		
+		//System.out.println(list[0][2]);
+		
+		System.out.println(Arrays.deepToString(list));
+		
+		//print all vegetables using a loop
+
+	}
+
+}
