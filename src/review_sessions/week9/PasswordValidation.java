84e9ee78ab422cd438c4a38466cc8b783418a3e7
europe <europe@192.168.1.153>
2020-07-25 12:30:09 -0400

    EU3

---
+++ src/review_sessions/week9/PasswordValidation.java
@@ -0,0 +1,112 @@
+package review_sessions.week9;
+
+public class PasswordValidation {
+
+	public static void main(String[] args) {
+		
+		/*
+		 Write a return method that can verify if a password is valid or not
+		 Requirements:
+		 1-Password MUST be at least have 6 characters and should not contain space
+		 2-Password should at least contain one upper case letter
+		 3-Password should at least contain one lowercase letter
+		 4-Password should at least contain one special character
+		 5-Password should at least contain a digit
+		 If All requirements above met, the method returns true, otherwise returns false
+		 */
+
+	}
+	
+	
+	public static String validatePassword(String str) {
+		
+		
+		//apple xxxx
+		boolean lenght = false;
+		boolean space = true;
+		boolean lower = false;
+		boolean upper = false;
+		boolean digit = false;
+		boolean special = false;
+		
+		
+		if(str.length()>=6) {
+			lenght=true;
+		}
+		
+		for(int i=0;i<str.length();i++) {
+			if(Character.isSpaceChar(str.charAt(i))) {
+				space = false;
+				break;
+			}
+		}
+		
+		for(int i=0;i<str.length();i++) {
+			if(Character.isLowerCase(str.charAt(i))) {
+				lower = true;
+				break;
+			}
+		}
+		
+		for(int i=0;i<str.length();i++) {
+			if(Character.isUpperCase(str.charAt(i))) {
+				upper= true;
+				break;
+			}
+		}
+		
+		for(int i=0;i<str.length();i++) {
+			if(Character.isDigit(str.charAt(i))) {
+				digit=true;
+				break;
+			}
+		}
+		
+		//special character
+		for(int i=0;i<str.length();i++) {
+			if(!Character.isLetterOrDigit(str.charAt(i)) && !Character.isSpace(str.charAt(i))) {
+				special=true;
+				break;
+			}
+		}
+	
+		if(lenght && space && lower && upper && digit && special) {
+			return "valid";
+		}else {
+			return "not valid";
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
