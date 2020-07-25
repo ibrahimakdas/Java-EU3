6d9c2faa04853633b6c32411df4ceb0439b0cbc1
europe <europe@192.168.1.153>
2020-07-25 14:26:09 -0400

    EU3

---
+++ src/review_sessions/week9/ArraysAsList.java
@@ -0,0 +1,24 @@
+package review_sessions.week9;
+
+import java.util.*;
+
+
+public class ArraysAsList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(10,40,12,3,5,7));
+
+		nums.remove(0);
+		nums.add(2);
+		System.out.println(nums);
+		
+		List<Integer> num2 = new ArrayList<>(Arrays.asList(10,20,30));
+		
+		
+		
+		
+		
+	}
+
+}
