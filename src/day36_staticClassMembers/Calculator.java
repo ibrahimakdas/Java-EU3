33dc6b8c5e9233d1ae8906ea1108811ce5929f4e
europe <europe@192.168.1.153>
2020-08-12 13:45:29 -0400

    EU3

--- src/day36_staticClassMembers/Calculator.java
+++ src/day36_staticClassMembers/Calculator.java
@@ -2,6 +2,8 @@ package day36_staticClassMembers;
 
 public class Calculator {
 	
+	int x;
+	
 	public static double plus(double num1,double num2) {
 		return num1+num2;
 	}
@@ -12,6 +14,10 @@ public class Calculator {
 	
 	public static double multiply(double num1,double num2) {
 		return num1*num2;
+	}
+	
+	public int m1() {
+		return x+4;
 	}
 
 }
--- src/day36_staticClassMembers/CalculatorTest.java
+++ src/day36_staticClassMembers/CalculatorTest.java
@@ -4,6 +4,14 @@ public class CalculatorTest {
 
 	public static void main(String[] args) {
 		
+		Calculator c1 = new Calculator();
+		System.out.println(c1.plus(2, 4));
+		
+		System.out.println(Calculator.plus(2, 4));
+		
+		int i = Integer.parseInt("33");
+		System.out.println(Character.isDigit('4'));
+		
 		
 
 	}
