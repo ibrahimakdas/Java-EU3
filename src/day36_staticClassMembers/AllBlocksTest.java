d56ad0e3f02589f450aa69a1c68841474d1f59e5
europe <europe@192.168.1.153>
2020-08-12 16:24:57 -0400

    EU3

--- src/day36_staticClassMembers/AllBlocksTest.java
+++ src/day36_staticClassMembers/AllBlocksTest.java
@@ -4,7 +4,12 @@ public class AllBlocksTest {
 
 	public static void main(String[] args) {
 		
+		//run static blocks - 1 time
+		
+		//run all instance blocoks
 		AllBlocks o1 = new AllBlocks();
+		
+		//run all instance blocks
 		AllBlocks o2 = new AllBlocks(8);
 
 	}
