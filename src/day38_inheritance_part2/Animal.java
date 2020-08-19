a41da4d5b41f54096a679c6f0a3dd2be0524c066
europe <europe@192.168.1.153>
2020-08-19 13:25:05 -0400

    EU3

---
+++ src/day38_inheritance_part2/Animal.java
@@ -0,0 +1,13 @@
+package day38_inheritance_part2;
+
+public class Animal {
+	
+	private String name;
+	private int brain;
+	private int body;
+	private int size;
+	private int weight;
+	
+	
+
+}
--- src/day38_inheritance_part2/SuperClass.java
+++ src/day38_inheritance_part2/SuperClass.java
@@ -3,8 +3,12 @@ package day38_inheritance_part2;
 public class SuperClass {
 	
 	public SuperClass() {
+		
 		System.out.println("This is the super class Constructor");
+		
 	}
+	
+	
 	
 
 }
