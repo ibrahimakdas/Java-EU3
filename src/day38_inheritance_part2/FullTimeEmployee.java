281362cefe4bee6f27313b8570c23353d8077ba6
europe <europe@192.168.1.153>
2020-08-19 15:53:16 -0400

    EU3

--- src/day38_inheritance_part2/FullTimeEmployee.java
+++ src/day38_inheritance_part2/FullTimeEmployee.java
@@ -2,4 +2,13 @@ package day38_inheritance_part2;
 
 public class FullTimeEmployee extends Employee {
 
+	@Override
+	void calculatePay(int hours, double rate) {
+		double total = (hours * rate)*1.05;
+		System.out.println("FullTime Emplyee Total Pay:" + total);
+	}
+	
+	
+	
+	
 }
