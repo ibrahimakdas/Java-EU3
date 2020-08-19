d8776020c634125bf688255d7da52699bad1e585
europe <europe@192.168.1.153>
2020-08-19 16:22:18 -0400

    EU3

--- src/day38_inheritance_part2/Animal.java
+++ src/day38_inheritance_part2/Animal.java
@@ -23,6 +23,10 @@ public class Animal {
 	public void eat() {
 		System.out.println("Animal.eat() is called");
 	}
+	
+	public void move(int speed) {
+		System.out.println("Animal.move() called. Animal is moving at " + speed);
+	}
 
 
 
