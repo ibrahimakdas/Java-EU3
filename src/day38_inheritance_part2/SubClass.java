00ec00448dfa87728d0912dea801c0be5e13503f
europe <europe@192.168.1.153>
2020-08-19 12:57:29 -0400

    EU3

---
+++ src/day38_inheritance_part2/SubClass.java
@@ -0,0 +1,11 @@
+package day38_inheritance_part2;
+
+public class SubClass extends SuperClass {
+	
+	public SubClass() {
+		super();
+		System.out.println("This is the subclass constructor");
+	}
+	
+	
+}
---
+++ src/day38_inheritance_part2/SuperClass.java
@@ -0,0 +1,10 @@
+package day38_inheritance_part2;
+
+public class SuperClass {
+	
+	public SuperClass() {
+		System.out.println("This is the super class Constructor");
+	}
+	
+
+}
