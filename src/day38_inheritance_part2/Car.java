608f775ad34b2d08f845f666b333b2cc93e2cfd1
europe <europe@192.168.1.153>
2020-08-19 16:09:54 -0400

    EU3

---
+++ src/day38_inheritance_part2/Car.java
@@ -0,0 +1,11 @@
+package day38_inheritance_part2;
+
+public class Car extends Vehicle {
+	
+	int maxSpeed = 180;
+	
+	public void display() {
+		System.out.println("maximum speed:" + maxSpeed);
+	}
+
+}
