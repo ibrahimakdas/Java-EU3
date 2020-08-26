f076574b39c829220a17385c37230389fd391608
europe <europe@192.168.1.153>
2020-08-26 13:40:06 -0400

    EU3

---
+++ src/day40_accessmodifiers_final/FinalMethods.java
@@ -0,0 +1,19 @@
+package day40_accessmodifiers_final;
+
+public class FinalMethods {
+	
+	public void method1() {
+		System.out.println("Method-1");
+	}
+	
+	public static void staticMethod(String word) {
+		System.out.println("Static Method");
+	}
+	
+}
+
+
+class Sub extends FinalMethods{
+	
+	
+}
