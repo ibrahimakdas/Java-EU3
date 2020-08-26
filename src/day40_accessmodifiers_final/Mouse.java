30d63133e81a1ea5002e91e19aa42d759ee0aa9b
europe <europe@192.168.1.153>
2020-08-26 15:34:01 -0400

    EU3

--- src/day40_accessmodifiers_final/Mouse.java
+++ src/day40_accessmodifiers_final/Mouse.java
@@ -5,7 +5,7 @@ public class Mouse extends Rodent {
 	protected int tailLength = 8;
 	
 	public void getMouseDetails() {
-		System.out.println("tail:" + tailLength + ",parentTail:" + tailLength);
+		System.out.println("tail:" + tailLength + ",parentTail:" + super.tailLength);
 	}
 	
 
