0f436a988a95b7b846e5e0f44e5086a558673860
europe <europe@192.168.1.153>
2020-08-26 15:53:30 -0400

    EU3

---
+++ src/day40_accessmodifiers_final/KangarooTest.java
@@ -0,0 +1,12 @@
+package day40_accessmodifiers_final;
+
+public class KangarooTest {
+
+	public static void main(String[] args) {
+		
+		Kangaroo k = new Kangaroo();
+		k.getMarsupialDescription();
+
+	}
+
+}
