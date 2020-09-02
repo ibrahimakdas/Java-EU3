fc5a0b3bf02a7146417309ed8cbe8be0ae9d63c1
europe <europe@192.168.1.153>
2020-09-02 15:14:49 -0400

    EU3

---
+++ src/day42_interface/Electric.java
@@ -0,0 +1,11 @@
+package day42_interface;
+
+public interface Electric {
+	
+	public abstract void charge();
+	
+	void charge2(); //public abstract 
+	
+	
+
+}
---
+++ src/day42_interface/Tesla2.java
@@ -0,0 +1,19 @@
+package day42_interface;
+
+public class Tesla2 implements Electric {
+
+	@Override
+	public void charge() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void charge2() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
