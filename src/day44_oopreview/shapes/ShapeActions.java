6cbe166c666a08da0d97dbbdd2e60b10141a810d
europe <europe@192.168.1.153>
2020-09-09 16:27:53 -0400

    EU3

---
+++ src/day44_oopreview/shapes/ShapeActions.java
@@ -0,0 +1,29 @@
+package day44_oopreview.shapes;
+
+import static day44_oopreview.shapes.ShapeManager.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeActions {
+	
+	public static void main(String[] args) {
+		
+		drawSquare((Square)buildShape("square"));
+		
+		drawShape(buildShape("shape"));
+		
+		List<Shape> myList = new ArrayList<>();
+		myList.add(new Shape());
+		myList.add(new Square());
+		myList.add(new Shape());
+		myList.add(new Square());
+		myList.add(new Diamond());
+		
+		drawShape(myList);
+		
+		
+		
+	}
+
+}
