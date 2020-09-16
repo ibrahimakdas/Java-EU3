ab64545231521edced137c896fd72c5dae038847
europe <europe@192.168.1.153>
2020-09-16 16:14:56 -0400

    EU3

--- src/day46_collection_part1/SortingList.java
+++ src/day46_collection_part1/SortingList.java
@@ -1,8 +1,19 @@
 package day46_collection_part1;
 
+import java.util.*;
+
 public class SortingList {
 
 	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		list.add("java");
+		list.add("js");
+		list.add("html");
+		list.add("ruby");
+		list.add("kotlin");
+		list.add("c++");
+		
 		
 		
 
