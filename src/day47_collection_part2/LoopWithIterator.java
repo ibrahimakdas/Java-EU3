59d4bab5f8dc229a4084eb8d3c1b8354c15214f4
europe <europe@192.168.1.153>
2020-09-22 16:10:52 -0400

    EU3

--- src/day47_collection_part2/LoopWithIterator.java
+++ src/day47_collection_part2/LoopWithIterator.java
@@ -1,6 +1,7 @@
 package day47_collection_part2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class LoopWithIterator {
@@ -16,6 +17,73 @@ public class LoopWithIterator {
 		nums.add(93);
 		nums.add(5000);
 		nums.add(950);
+		
+		Iterator<Integer> it = nums.iterator();
+		
+		//call hasNext() method
+		//hasNext() return  true, if there is still next value 
+		//returns false when it reaches the end or empty
+		
+		System.out.println("it.hasNext():" + it.hasNext());
+		System.out.println("it.next():" + it.next());
+		System.out.println("it.next():" + it.next());
+		System.out.println("it.next():" + it.next());
+		System.out.println("it.next():" + it.next());
+		System.out.println("it.next():" + it.next());
+		
+		System.out.println();
+		
+		while(it.hasNext()) {
+			System.out.print(it.next() + "|");
+		}
+		
+	//	System.out.println(it.next());
+		
+		
+		Iterator<Integer> it2 = nums.iterator();
+		
+		
+		//What is the difference between for each loop and iterator
+		//we can remove objects while looping
+		
+		while(it2.hasNext()) {
+			int val = it2.next();
+			if(val > 1000) {
+				it2.remove();
+			}
+		}
+		
+		List<String> cities = new ArrayList<>();
+		cities.add("New York");
+		cities.add("Boston");
+		cities.add("Virginia");
+		cities.add("DC");
+		
+		for(String city : cities) {
+			if(city.equals("Boston")) {
+				cities.remove(city);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
