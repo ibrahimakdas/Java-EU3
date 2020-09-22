c4c4c04cd53dbf503fad6a7215994f4c69b8d799
europe <europe@192.168.1.153>
2020-09-22 15:43:54 -0400

    EU3

---
+++ src/day47_collection_part2/LoopArrayList.java
@@ -0,0 +1,23 @@
+package day47_collection_part2;
+
+import java.util.*;
+
+public class LoopArrayList {
+
+	public static void main(String[] args) {
+		
+		List<Integer> nums = new ArrayList<>();
+		nums.add(50);
+		nums.add(543);
+		nums.add(1150);
+		nums.add(550);
+		nums.add(570);
+		nums.add(93);
+		nums.add(5000);
+		nums.add(950);
+		
+		
+
+	}
+
+}
