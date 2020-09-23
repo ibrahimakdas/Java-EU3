d6526a7bfbd63bd5f517851ea35e274d40167e08
europe <europe@192.168.1.153>
2020-09-23 13:35:53 -0400

    EU3

--- src/day48_collection_part3/MapExample.java
+++ src/day48_collection_part3/MapExample.java
@@ -13,7 +13,8 @@ public class MapExample {
 		items.put("Flowers", 10.99);
 		items.put("Eggs", 4.99);
 		items.put("Milk", 6.2);
-		items.put("Cherries", 4.99);
+		items.put("Cherries", 10.99);
+		items.put("Apple", 5.49);
 		
 		System.out.println(items.size());
 		
@@ -28,6 +29,23 @@ public class MapExample {
 		items.remove("Flowers");
 		
 		System.out.println(items.toString());
+		
+		System.out.println("Is Apples there? " + items.containsKey("Apple"));
+		System.out.println("Is Flower there? " + items.containsKey("Flowers"));
+		
+		items.replace("Eggs", 3.5);
+		System.out.println("Price of eggs:" + items.get("Eggs"));
+		
+		//increase price of cherries by $2
+		items.replace("Cherries", items.get("Cherries") + 2);
+		System.out.println("Price of cherries: " + items.get("Cherries"));
+		
+		
+		
+		
+		
+		
+	
 		
 		
 		
