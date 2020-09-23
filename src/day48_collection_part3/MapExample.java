30a634efd328f3b57fa779351770c104a42a20d7
europe <europe@192.168.1.153>
2020-09-23 13:21:35 -0400

    EU3

---
+++ src/day48_collection_part3/MapExample.java
@@ -0,0 +1,21 @@
+package day48_collection_part3;
+
+import java.util.*;
+
+
+public class MapExample {
+
+	public static void main(String[] args) {
+		
+		Map<String,Double> items = new HashMap<>();
+		
+		items.put("Apple", 3.49);
+		items.put("Flowers", 10.99);
+		items.put("Eggs", 4.99);
+		items.put("Milk", 6.2);
+		items.put("Cherries", 4.99);
+		
+
+	}
+
+}
