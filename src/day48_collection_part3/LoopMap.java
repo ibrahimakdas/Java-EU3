a1373a8ccb73d3fffe94b6e63820f59e3bc13ea0
europe <europe@192.168.1.153>
2020-09-23 13:46:12 -0400

    EU3

---
+++ src/day48_collection_part3/LoopMap.java
@@ -0,0 +1,30 @@
+package day48_collection_part3;
+
+import java.util.*;
+
+public class LoopMap {
+
+	public static void main(String[] args) {
+		
+		Map<String,String> dataMap = new HashMap<>();
+		
+		dataMap.put("url", "qa3.vytrack.com");
+		dataMap.put("username", "user66");
+		dataMap.put("password", "abc66");
+		dataMap.put("browser", "chrome");
+		dataMap.put("truckDriver", "Bob");
+		dataMap.put("storeManager", "John");
+		
+		//keySet - > returns set with all the keys in the map 
+		//values - > returns all the values in the map
+		
+		System.out.println("++++KEYS++++");
+		for(String key : dataMap.keySet()) {
+			System.out.print(key + "|");
+		}
+		
+				
+
+	}
+
+}
