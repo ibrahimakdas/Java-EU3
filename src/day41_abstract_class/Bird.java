f17778266ea3e77f814dd79b676a56885439a327
ibrahimakdas <iaibrahimakdas@gmail.com>
2020-10-06 22:53:59 +0300

    push  it

--- src/day41_abstract_class/Bird.java
+++ src/day41_abstract_class/Bird.java
@@ -3,6 +3,11 @@ package day41_abstract_class;
 public abstract class Bird extends Animal {
 
 
+	public Bird(int age) {
+		super(age);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	void eat() {
 		System.out.println("Bird is eating");
--- src/day41_abstract_class/Dog.java
+++ src/day41_abstract_class/Dog.java
@@ -3,6 +3,12 @@ package day41_abstract_class;
 public class Dog extends Animal{
 
 	
+	public Dog(int age) {
+		super(age);
+		// TODO Auto-generated constructor stub
+	}
+
+
 	@Override
 	void eat() {
 		System.out.println("Dog food");
